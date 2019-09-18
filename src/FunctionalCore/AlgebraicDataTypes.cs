f7d2ff3562ef0b7f479f51414549c2f306552b56
Pavlo <pavlo.grushetsky@gmail.com>
2019-09-18 18:56:12 +0300

    The Union3 was refactored, and equality was implemented.

--- src/FunctionalCore/AlgebraicDataTypes.cs
+++ src/FunctionalCore/AlgebraicDataTypes.cs
@@ -105,40 +105,141 @@ namespace FunctionalCore
         #endregion
     }
 
+    /// <summary>
+    /// The discriminated union with three union cases.
+    /// </summary>
+    /// <typeparam name="T1">The type of the first union case.</typeparam>
+    /// <typeparam name="T2">The type of the second union case.</typeparam>
+    /// <typeparam name="T3">The type of the third union case.</typeparam>
     public abstract class Union3<T1, T2, T3>
     {
+        /// <summary>
+        /// Match the cases of the discriminated union with the corresponding functions.
+        /// </summary>
+        /// <typeparam name="T">The type of the result value.</typeparam>
+        /// <param name="f1">The function to match with the first union case.</param>
+        /// <param name="f2">The function to match with the second union case.</param>
+        /// <param name="f3">The function to match with the third union case.</param>
+        /// <returns>The result value.</returns>
         public abstract T Match<T>(Func<T1, T> f1, Func<T2, T> f2, Func<T3, T> f3);
+
+        /// <summary>
+        /// Match the cases of the discriminated union with the corresponding actions.
+        /// </summary>
+        /// <param name="f1">The action to match with the first union case.</param>
+        /// <param name="f2">The action to match with the second union case.</param>
+        /// <param name="f3">The action to match with the third union case.</param>
         public abstract void Match(Action<T1> f1, Action<T2> f2, Action<T3> f3);
 
         private Union3() { }
 
-        public sealed class Case1 : Union3<T1, T2, T3>
+        internal sealed class UnionCase1 : Union3<T1, T2, T3>
         {
-            public readonly T1 Item;
-            internal Case1(T1 item) => Item = item;
-            public override T Match<T>(Func<T1, T> f1, Func<T2, T> f2, Func<T3, T> f3) => f1 != null ? f1(Item) : default;
-            public override void Match(Action<T1> f1, Action<T2> f2, Action<T3> f3) => f1?.Invoke(Item);
+            internal readonly T1 Item;
+
+            internal UnionCase1(T1 item) 
+                => Item = item;
+
+            /// <inheritdoc />
+            public override T Match<T>(Func<T1, T> f1, Func<T2, T> f2, Func<T3, T> f3) 
+                => f1 != null 
+                    ? f1(Item) 
+                    : default;
+
+            /// <inheritdoc />
+            public override void Match(Action<T1> f1, Action<T2> f2, Action<T3> f3) 
+                => f1?.Invoke(Item);
         }
 
-        public sealed class Case2 : Union3<T1, T2, T3>
+        internal sealed class UnionCase2 : Union3<T1, T2, T3>
         {
-            public readonly T2 Item;
-            internal Case2(T2 item) => Item = item;
-            public override T Match<T>(Func<T1, T> f1, Func<T2, T> f2, Func<T3, T> f3) => f2 != null ? f2(Item) : default;
-            public override void Match(Action<T1> f1, Action<T2> f2, Action<T3> f3) => f2?.Invoke(Item);
+            internal readonly T2 Item;
+
+            internal UnionCase2(T2 item) 
+                => Item = item;
+
+            /// <inheritdoc />
+            public override T Match<T>(Func<T1, T> f1, Func<T2, T> f2, Func<T3, T> f3) 
+                => f2 != null 
+                    ? f2(Item) 
+                    : default;
+
+            /// <inheritdoc />
+            public override void Match(Action<T1> f1, Action<T2> f2, Action<T3> f3) 
+                => f2?.Invoke(Item);
         }
 
-        public sealed class Case3 : Union3<T1, T2, T3>
+        internal sealed class UnionCase3 : Union3<T1, T2, T3>
         {
-            public readonly T3 Item;
-            internal Case3(T3 item) => Item = item;
-            public override T Match<T>(Func<T1, T> f1, Func<T2, T> f2, Func<T3, T> f3) => f3 != null ? f3(Item) : default;
-            public override void Match(Action<T1> f1, Action<T2> f2, Action<T3> f3) => f3?.Invoke(Item);
+            internal readonly T3 Item;
+
+            internal UnionCase3(T3 item) 
+                => Item = item;
+
+            /// <inheritdoc />
+            public override T Match<T>(Func<T1, T> f1, Func<T2, T> f2, Func<T3, T> f3) 
+                => f3 != null 
+                    ? f3(Item) 
+                    : default;
+
+            /// <inheritdoc />
+            public override void Match(Action<T1> f1, Action<T2> f2, Action<T3> f3) 
+                => f3?.Invoke(Item);
         }
 
-        public static Union3<T1, T2, T3> Case(T1 value) => new Case1(value);
-        public static Union3<T1, T2, T3> Case(T2 value) => new Case2(value);
-        public static Union3<T1, T2, T3> Case(T3 value) => new Case3(value);
+        /// <summary>
+        /// Construct a value of the discriminated union using the first union case.
+        /// </summary>
+        /// <param name="value">The value of the first union case.</param>
+        /// <returns>The discriminated union value.</returns>
+        public static Union3<T1, T2, T3> Case1(T1 value) 
+            => new UnionCase1(value);
+
+        /// <summary>
+        /// Construct a value of the discriminated union using the second union case.
+        /// </summary>
+        /// <param name="value">The value of the second union case.</param>
+        /// <returns>The discriminated union value.</returns>
+        public static Union3<T1, T2, T3> Case2(T2 value) 
+            => new UnionCase2(value);
+
+        /// <summary>
+        /// Construct a value of the discriminated union using the third union case.
+        /// </summary>
+        /// <param name="value">The value of the third union case.</param>
+        /// <returns>The discriminated union value.</returns>
+        public static Union3<T1, T2, T3> Case3(T3 value) 
+            => new UnionCase3(value);
+
+        #region Equality
+
+        public override bool Equals(object obj)
+            => obj is Union3<T1, T2, T3> other
+               && Equals(other);
+
+        protected bool Equals(Union3<T1, T2, T3> other)
+            => other != null
+               && other.Match(
+                   otherCase1 => Match(
+                       thisCase1 => otherCase1.Equals(thisCase1),
+                       thisCase2 => false,
+                       thisCase3 => false),
+                   otherCase2 => Match(
+                       thisCase1 => false,
+                       thisCase2 => otherCase2.Equals(thisCase2),
+                       thisCase3 => false),
+                   otherCase3 => Match(
+                       thisCase1 => false,
+                       thisCase2 => false,
+                       thisCase3 => otherCase3.Equals(thisCase3)));
+
+        public override int GetHashCode()
+            => Match(
+                case1 => case1.GetHashCode(),
+                case2 => case2.GetHashCode(),
+                case3 => case3.GetHashCode());
+
+        #endregion
     }
 
     #endregion
