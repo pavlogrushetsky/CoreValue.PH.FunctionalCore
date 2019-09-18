65d1c6a3483edd961b64bee39048541865092413
Pavlo <pavlo.grushetsky@gmail.com>
2019-09-18 14:53:11 +0300

    Unit tests were added to the DiscriminatedUnionsTests.cs for the discriminated unions with two cases.

--- tests/FunctionalCore.Tests/DiscriminatedUnionsTests.cs
+++ tests/FunctionalCore.Tests/DiscriminatedUnionsTests.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 
 namespace FunctionalCore.Tests
 {
-    public class Tests
+    public class DiscriminatedUnionsTests
     {
         [Test]
         public void TwoUnionsWithTheSameTypeAndCaseAndValueShouldBeEqual() 
@@ -17,9 +17,31 @@ namespace FunctionalCore.Tests
                 Union2<int, string>.Case1(2));
 
         [Test]
+        public void TwoUnionsWithTheSameTypeButDifferentCaseShouldNotBeEqual()
+            => Assert.AreNotEqual(
+                Union2<int, string>.Case1(1),
+                Union2<int, string>.Case2("1"));
+
+        [Test]
         public void TwoUnionCasesWithTheDifferentTypesShouldNotBeEqual() 
             => Assert.AreNotEqual(
                 Union2<int, string>.Case1(1),
                 Union2<string, int>.Case2(1));
+
+        [Test]
+        public void MatchShouldInvokeTheFunctionForTheFirstCase()
+            => Assert.IsTrue(
+                Union2<int, string>.Case1(1)
+                    .Match(
+                        case1 => true, 
+                        case2 => false));
+
+        [Test]
+        public void MatchShouldInvokeTheFunctionForTheSecondCase()
+            => Assert.IsTrue(
+                Union2<int, string>.Case2("1")
+                    .Match(
+                        case1 => false,
+                        case2 => true));
     }
 }
